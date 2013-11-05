4eadd67d6bfae9bf6f3f34c48f1eec7b2b4e9175
mattschneider2002 <matts3@u.washington.edu>
2013-11-04 21:53:34 -0800

    Rulers 0.0.2

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,13 +1,30 @@
 # rulers/lib/rulers.rb
 require "rulers/version"
 require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   class Application
     def call (env)
+      if env['PATH_INFO'] == '/favicon.ico'
+        return [404, {'Content-Type' => 'text/html'}, []]
+      end
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
       `echo debug > debug.txt`;
         [200, {'Content-Type' => 'text/html'},
-          ['Hello from Ruby on Rulers!']]
+          [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
